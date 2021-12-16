3d438b60541cf83dd1a36b5a7100322f7a0eb5d1
Saloni Varshney <saloni.varshney@sap.com>
2021-12-16 19:53:19 +0530

    Adding new Screens

--- e-Bookshop/lib/main.dart
+++ e-Bookshop/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:food_app/config/colors.dart';
+import 'package:food_app/providers/product_provider.dart';
+import 'package:food_app/providers/user_provider.dart';
+import 'package:food_app/screens/home/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
